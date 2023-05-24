f9687e06e3d3d89a6a75e3037d11ba4226ed9714
Akhil Divi <akdivi2025@student.amsacs.org>
2023-05-24 10:19:04 -0400

    Created SwingTest class to test mouseListener

--- SwingTest.java
+++ SwingTest.java
@@ -1,13 +1,45 @@
 import javax.swing.*;
-import java.awt.*;
-public class SwingTest {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("minesweeper");
+import java.awt.event.*;
+import java.util.*;
+public class SwingTest extends JFrame implements MouseListener {
+    JFrame frame;
+    JButton button;
+    public SwingTest() {
+        this.frame = new JFrame("minesweeper");
+        frame.addMouseListener(this);
         frame.setSize(1000, 800);
         frame.setVisible(true);
-        JButton button = new JButton("click me");
+        this.button = new JButton("click me");
+        button.setEnabled(true);
+        button.setBounds(0,0,50, 50);
         frame.add(button);
-        button.setSize(100, 100);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
         
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        button.setVisible(false);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event){
+        
+    }
+    public static void main(String[] args) {
+        SwingTest minesweeper = new SwingTest();
     }
 }
